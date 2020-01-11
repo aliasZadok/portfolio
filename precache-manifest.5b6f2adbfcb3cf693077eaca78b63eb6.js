cb7839fcd1dac6e21c0945c483796fe0c270b2e0
aliasZadok <lfstone3@gmail.com>
2020-01-12 01:50:40 +0530

    Updates

---
+++ precache-manifest.5b6f2adbfcb3cf693077eaca78b63eb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac20b45c36fa6c42ca77400b91b563b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efad5b91f7551b12a7af",
+    "url": "/static/js/2.eb3c4b82.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.eb3c4b82.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4ac8efcb5ee9eb30daa",
+    "url": "/static/js/main.9a434325.chunk.js"
+  },
+  {
+    "revision": "c0d7c58e510620f2f64c",
+    "url": "/static/js/runtime-main.7b61e083.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ac3cac6bba085e56eccea711a1c64372.js"
+  "/precache-manifest.5b6f2adbfcb3cf693077eaca78b63eb6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
