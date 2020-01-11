f163ec6ebab8d8f7da8cd3d8383f4f2e50e8e34e
aliasZadok <lfstone3@gmail.com>
2020-01-12 00:48:13 +0530

    Updates

---
+++ precache-manifest.155ade9ae4f3acc214f03883d8295452.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29cfb8ffb606d24e5c452d18145baab8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efad5b91f7551b12a7af",
+    "url": "/static/js/2.eb3c4b82.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.eb3c4b82.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80d532b319f18049c93c",
+    "url": "/static/js/main.6869efd0.chunk.js"
+  },
+  {
+    "revision": "c0d7c58e510620f2f64c",
+    "url": "/static/js/runtime-main.7b61e083.js"
+  }
+]);
